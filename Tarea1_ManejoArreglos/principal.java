a7491e8b0ff47a9b2b5856c7de76e3da13a589d0
José Eduardo Hernández <102254199+Jose-Eduardo-Hrz@users.noreply.github.com>
2022-03-24 21:37:18 -0600

    Update principal.java

--- Tarea1_ManejoArreglos/principal.java
+++ Tarea1_ManejoArreglos/principal.java
@@ -7,42 +7,34 @@ public class principal{
         Scanner entrada = new Scanner( System.in );
 
         // Se obtiene el tamaño del arreglo y se almacena en la variable tamArreglo
-        System.out.println("Indica cuantos nombres quieres registrar:");
+        System.out.println("\u001B[33mIndica cuantos nombres quieres registrar:\u001B[0m");
         int tamArreglo = Integer.parseInt( entrada.nextLine() );
         System.out.println();
 
         // Arreglo de tipo String que almacena nombres
         String nombres [] = new String[ tamArreglo ];
 
-        System.out.println("Proporciona los " + tamArreglo + " nombres: ");
+        System.out.println("\u001B[33mProporciona los " + tamArreglo + " nombres: \u001B[0m");
         // Ciclo que permite obtener los nombres
         for( int i = 0 ; i < tamArreglo ; i++ ){
             nombres[i] = entrada.nextLine();
         }
         System.out.println();
 
-        // Ciclo que permite imprimir los nombres del arreglo
-        System.out.println( "Nombres almacenados en el arreglo: " );
-        for( String nombre : nombres ){
-            System.out.print( nombre + " " );
-        }
-        System.out.println('\n');
+        // Metodo que permite imprimir los nombres del arreglo
+        imprimirArreglo(nombres);
 
         // Variable que almacena el nombre con menor número de caracteres
         // (Al inicio se indica la posicion 0 del arreglo para tener un comparador)
         String menorCaracteres = nombres[0];
 
-        // Ciclo que permite obtener el nombre con menor caracteres
-        for( int i = 1 ; i < tamArreglo ; i++ ){
-            menorCaracteres = ( nombres[i].length() < menorCaracteres.length() ? nombres[i] : menorCaracteres );
-        }
-
         // Variable que almacena el nombre con mayor número de caracteres
         // (Al inicio se indica la posicion 0 del arreglo para tener un comparador)
         String mayorCaracteres = nombres[0];
 
-        // Ciclo que permite obtener el nombre con menor caracteres
+        // Ciclo que permite obtener el nombre con menor caracteres y mayor caracteres
         for( int i = 1 ; i < tamArreglo ; i++ ){
+            menorCaracteres = ( nombres[i].length() < menorCaracteres.length() ? nombres[i] : menorCaracteres );
             mayorCaracteres = ( nombres[i].length() > mayorCaracteres.length() ? nombres[i] : mayorCaracteres );
         }
 
@@ -60,16 +52,23 @@ public class principal{
         }
 
         // Seccion para mostrar los resultados
-        System.out.println( "El nombre con menor caracteres es: " + menorCaracteres );
-        System.out.println( "El nombre con menor caracteres es: " + mayorCaracteres + "\n" );
+        System.out.println( "\u001B[33mEl nombre con menor caracteres es: \u001B[0m" + menorCaracteres );
+        System.out.println( "\u001B[33mEl nombre con menor caracteres es: \u001B[0m" + mayorCaracteres + "\n" );
 
-        System.out.println( "Arreglo ordenado alfabeticamente: " );
-        for( String nombre : nombres ){
-            System.out.print( nombre + " " );
-        }
-        System.out.println();
+        // Metodo que permite imprimir los nombres del arreglo
+        imprimirArreglo(nombres);
 
         entrada.close();
 
     }
+
+    // Metodo para imprimir los datos del arreglo
+    public static void imprimirArreglo( String nombres [] ){
+        System.out.println( "\u001B[33mNombres almacenados en el arreglo: \u001B[0m" );
+        for( String nombre : nombres ){
+            System.out.print( nombre + " " );
+        }
+        System.out.println('\n');
+    }
+
 }
