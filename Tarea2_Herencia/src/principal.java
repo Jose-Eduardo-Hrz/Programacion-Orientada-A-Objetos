c73041c46957bbd3dc4af4612d8dcd97ae8ff3f5
José Eduardo Hernández <102254199+Jose-Eduardo-Hrz@users.noreply.github.com>
2022-03-31 16:49:02 -0600

    Update principal.java

--- Tarea2_Herencia/src/principal.java
+++ Tarea2_Herencia/src/principal.java
@@ -3,13 +3,63 @@ import java.util.Scanner;
 public class principal{
     public static void main(String[] args) {
 
-        Animal [] animales = new Animal[5];
+        Scanner entrada = new Scanner( System.in );
 
-        animales[0] = new Anfibio("Nombre", "tipoPiel", "tipoRespiracion", "tipoReproduccion", "habitad", "colorPiel");
-        animales[1] = new Ave("Nombre", "tipoPiel", "tipoRespiracion", "tipoReproduccion", "habitad", true);
-        animales[2] = new Mamifero("Nombre", "tipoPiel", "tipoRespiracion", "tipoReproduccion", "habitad", false);
-        animales[3] = new Pez("Nombre", "tipoPiel", "tipoRespiracion", "tipoReproduccion", "habitad", 3);
-        animales[4] = new Reptil("Nombre", "tipoPiel", "tipoRespiracion", "tipoReproduccion", "habitad", false);
+        System.out.println("Indica el numero de animales anfibios:");
+        int numAnfibios = Integer.parseInt(entrada.nextLine());
+
+        System.out.println("Indica el numero de animales que son aves:");
+        int numAves = Integer.parseInt(entrada.nextLine());
+
+        System.out.println("Indica el numero de animales mamiferos:");
+        int numMamiferos = Integer.parseInt(entrada.nextLine());
+
+        System.out.println("Indica el numero de animales que son peces:");
+        int numPeces = Integer.parseInt(entrada.nextLine());
+
+        System.out.println("Indica el numero de reptiles:");
+        int numReptiles = Integer.parseInt(entrada.nextLine());
+
+        int numTotalAnimales = numAnfibios + numAves + numMamiferos + numPeces + numReptiles;
+
+        Animal [] animales = new Animal[ numTotalAnimales ];
+
+        int i = 0;
+
+        System.out.println( "Proporcione los animales anfibios de la siguiente manera:" );
+        System.out.println( "\u001B[36m [Nombre: String] [tipoPiel: String] [tipoRespiracion: String] [tipoReproduccion: String] [habitad]: String [colorPiel: String] \u001B[0m" );
+        for( int j = 0 ; j < numAnfibios ; j++ ){
+            animales[i] = new Anfibio( entrada.next() , entrada.next(), entrada.next(), entrada.next(), entrada.next(), entrada.next());
+            i++;
+        }
+
+        System.out.println( "Proporcione las aves de la siguiente manera:" );
+        System.out.println( "\u001B[36m [Nombre: String] [tipoPiel: String] [tipoRespiracion: String] [tipoReproduccion: String] [habitad: String] [vuela: boolean]\u001B[0m" );
+        for( int j = 0 ; j < numAnfibios ; j++ ){
+            animales[i] = new Ave(entrada.next(), entrada.next(), entrada.next(), entrada.next(), entrada.next(), Boolean.parseBoolean(entrada.next()));
+            i++;
+        }
+
+        System.out.println( "Proporcione los animeles mamiferos de la siguiente manera:" );
+        System.out.println( "\u001B[36m [Nombre: String] [tipoPiel: String] [tipoRespiracion: String] [tipoReproduccion: String] [habitad: String] [terrestre: boolean]\u001B[0m" );
+        for( int j = 0 ; j < numAnfibios ; j++ ){
+            animales[i] = new Mamifero(entrada.next(), entrada.next(), entrada.next(), entrada.next(), entrada.next(), Boolean.parseBoolean(entrada.next()));
+            i++;
+        }
+
+        System.out.println( "Proporcione los peces de la siguiente manera:" );
+        System.out.println( "\u001B[36m [Nombre: String] [tipoPiel: String] [tipoRespiracion: String] [tipoReproduccion: String] [habitad: String] [numeroOjos: int]\u001B[0m" );
+        for( int j = 0 ; j < numAnfibios ; j++ ){
+            animales[i] = new Pez(entrada.next(), entrada.next(), entrada.next(), entrada.next(), entrada.next(), Integer.parseInt(entrada.next()));
+            i++;
+        }
+
+        System.out.println( "Proporcione los reptiles de la siguiente manera:" );
+        System.out.println( "\u001B[36m [Nombre: String] [tipoPiel: String] [tipoRespiracion: String] [tipoReproduccion: String] [habitad: String] [tienePatas: boolean]\u001B[0m" );
+        for( int j = 0 ; j < numAnfibios ; j++ ){
+            animales[i] = new Reptil(entrada.next(), entrada.next(), entrada.next(), entrada.next(), entrada.next(), Boolean.parseBoolean(entrada.next()));
+            i++;
+        }
 
         pintarTabla(animales);
     }
